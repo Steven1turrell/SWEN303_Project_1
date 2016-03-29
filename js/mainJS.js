3d86e0cbf165319bbe56e8d03560da38d9d20f44
Steven1turrell <steven1turrell@gmail.com>
2016-03-29 18:58:19 +1300

    Initial Commit

---
+++ js/mainJS.js
@@ -0,0 +1,13 @@
+/**
+ * Created by steve on 29/03/2016.
+ */
+
+var main = function() {
+    console.log("Hi");
+    $('#search-button').click(function() {
+        console.log("fuck");
+        $('.color-body').fadeOut(1000);
+    });
+};
+
+$(document).ready(main());
---
+++ js/xmlManager.js
@@ -0,0 +1,3 @@
+/**
+ * Created by steve on 26/03/2016.
+ */
